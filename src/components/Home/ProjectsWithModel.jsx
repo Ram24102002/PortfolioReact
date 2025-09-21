435f040237af823ac671c1660f1e821ba1bb9031
Ram24102002 <ramachandran24102002@gmail.com>
2025-09-21 21:08:51 +0530

    ProjectsWithModel mobile

--- src/components/Home/ProjectsWithModel.jsx
+++ src/components/Home/ProjectsWithModel.jsx
@@ -54,7 +54,7 @@ function ProjectsWithModel() {
     <div>
         {/* Header */}
         <ScrollFadeIn>
-        <div className="mb-12">
+        <div className="ml-4 mt-20 mb-12">
           <h1 className="text-5xl md:text-7xl font-bold text-white md:mb-2">RECENT</h1>
           <h2 className="text-5xl md:text-7xl font-bold text-gray-600">PROJECTS</h2>
         </div>
@@ -63,9 +63,9 @@ function ProjectsWithModel() {
 
         {projectsArray.map((project) =>(
         <ScrollFadeIn><div className="min-h-screen bg-black" key={project.id}>
-          <div className="flex">
+          <div className="flex flex-col md:flex-row">
             {/* Left Side - Main Content */}
-            <div className="w-1/3 p-12 ">
+            <div className="w-full md:w-1/3 p-12  pb-0">
               {/* Logo */}
               <div className="mb-16">
                 <div className="w-20 h-20 rounded-full bg-gradient-to-br from-orange-400 to-yellow-500 flex items-center justify-center flex-shrink-0">
@@ -74,16 +74,16 @@ function ProjectsWithModel() {
                             src={project.logo} alt="" />
                           </div>
                 
-                <h1 className="text-5xl text-white font-bold  mb-8 leading-tight">
+                <h1 className="text-4xl md:text-5xl text-white font-bold  mb-8 leading-tight">
                   {project.title}
                 </h1>
                 
-                <p className="text-xl  text-white   mb-12 leading-relaxed max-w-lg">
+                <p className="text-lg md:text-xl  text-white   mb-12 leading-relaxed max-w-lg">
                   {project.description} <span className='text-gray-400 text-sm'>{project.Completed ? "(Completed)" : "(Ongoing)"}</span>
                 </p>
     
                 <Link to={project.projectLink} target='_blank'>
-                    <button className="bg-lime-400   px-8 py-4 rounded-xl  font-medium hover:bg-lime-200  transition-colors shadow-sm">
+                    <button className="bg-lime-400  w-full px-8 py-4 rounded-xl  font-medium hover:bg-lime-200  transition-colors shadow-sm">
                         Visite the Project
                     </button>
                 </Link>
@@ -91,25 +91,31 @@ function ProjectsWithModel() {
             </div>
     
             {/* Right Side - App Interfaces */}
-            <div className="w-2/3 p-8  flex items-center justify-center relative">
+            <div className="w-full md:w-2/3 sm:h-[50dvh] md:h-[100dvh] p-8 flex items-center justify-center relative">
     
             
               {/* Mobile Phone */}
-              <div className='w-1/3 flex flex-col items-center justify-center mr-[-90px] z-10 mt-30'>
+              
+              <div className='w-1/3 flex flex-col items-center justify-center md:mr-[-90px] z-10 md:mt-30'>
                   <div className='h-95 w-45 border-4 border-white rounded-3xl shadow-xl overflow-hidden mb-8'>
-                    <img src={project.mobileImage} className='object-fit' alt="" />
+                    <Link to={project.projectLink}  target='_blank'>
+                      <img src={project.mobileImage} className='object-cover' alt="" />
+                    </Link>
                   </div>
               </div>
               
     
     
               {/* Desktop Browser Window */}
-              <div className="mockup-browser bg-white border-base-300 border w-2/3">
+              
+              <div className="mockup-browser bg-white border-base-300 border hidden md:block md:w-2/3 ">
       <div className="mockup-browser-toolbar p-2">
         <div className="input">{project.projectLink}</div>
       </div>
-      <div className="grid place-content-center border-t border-base-300 h-80">
+      <div className="grid place-content-center border-t border-base-300 h-50 md:h-80 ">
+        <Link to={project.projectLink} target='_blank'>
         <img src={project.desktopImage}  className=' py-5'  alt="" />
+        </Link>
       </div>
     </div>
     
